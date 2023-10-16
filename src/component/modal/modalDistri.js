07bdf68b2030a196571b7cc1f3f50e83c3be9f7d
thuan <lapthuan0805@gmail.com>
2023-10-16 13:16:55 +0700

    aaaaaaâ

--- src/component/modal/modalDistri.js
+++ src/component/modal/modalDistri.js
@@ -8,9 +8,9 @@ import {
     ProFormText,
     ProFormTextArea,
 } from '@ant-design/pro-components';
-import { Button, Card, Form, message } from 'antd';
+import { Button, Card, Empty, Form, message, Space } from 'antd';
 import { useEffect, useState } from 'react';
-import useAsync from '../../hook/useAsync';
+import { AiFillLock } from 'react-icons/ai';
 import ServiceDistributed from '../../service/ServiceDistributed';
 
 const ModalDistri = () => {
@@ -18,26 +18,18 @@ const ModalDistri = () => {
     const [bang, setBang] = useState()
     const [cot, setCot] = useState()
     const [dieukien, setDieukien] = useState("")
-    const [bangvitu1, setBangvitu1] = useState()
-    const [cotvitu1, setCotvitu1] = useState()
-    const [dieukienvitu1, setDieukienvitu1] = useState("")
+    const [columnOptionTable, setColumnOptionTable] = useState([])
     const [columnOption, setColumnOption] = useState([])
+    const [columnOptionDatabase, setColumnOptionDatabase] = useState([])
+    const [isLogin, setIsLogin] = useState(false)
+    const [host, setHost] = useState()
+    const [userName, setUserName] = useState()
+    const [password, setPassword] = useState()
+    const [database, setDataBase] = useState()
 
-    const { data: resTable } = useAsync(() => ServiceDistributed.ShowTable())
-
-    let tableOption = []
-
-    resTable?.table?.map((item, index) => {
-        tableOption.push({
-            value: item,
-            label: item,
-        })
-    })
 
     useEffect(() => {
-
         if (bang) {
-
             (async () => {
                 let columnsTemps = []
                 const resColumn = await ServiceDistributed.ShowColumn(bang)
@@ -50,10 +42,35 @@ const ModalDistri = () => {
                 })
                 setColumnOption(columnsTemps)
             })()
+        } else {
+            setColumnOption([])
         }
-
     }, [bang])
+    const loginMysql = async () => {
+      
+        const body = {
+            "host": host ? host : "",
+            "username": userName ? userName : "",
+            "password": password ? password : ""
+        }
+        const res = await ServiceDistributed.LoginMysql(body)
 
+        if (res.databases) {
+            let columnsTemps = []
+            res?.databases?.map((item, index) => {
+                columnsTemps.push({
+                    value: item,
+                    label: item,
+                })
+            })
+            setColumnOptionDatabase(columnsTemps)
+            message.success("Đăng nhập thành công")
+
+        } else {
+            setColumnOptionDatabase([])
+            message.warning("Đăng nhập thất bại, kiểm tra lại tài khoản và mật khẩu!")
+        }
+    }
     const waitTime = (time) => {
         return new Promise((resolve) => {
             setTimeout(() => {
@@ -61,11 +78,15 @@ const ModalDistri = () => {
             }, time);
         });
     };
-
+    const backFormLogin = () => {
+        if (isLogin === true) {
+            setIsLogin(false)
+        }
+    }
     return (
         <ModalForm
-
-            title="Phân tán dữ liệu"
+            width={isLogin === false && 500}
+            title="Phân tán dữ liệu" y
             trigger={
                 < Button type="dashed" >
                     Phân tán dữ liệu
@@ -75,98 +96,164 @@ const ModalDistri = () => {
             autoFocusFirstInput
             modalProps={{
                 destroyOnClose: true,
-                onCancel: () => console.log('run'),
+                onCancel: backFormLogin,
             }}
             submitTimeout={2000}
             onFinish={async (values) => {
                 await waitTime(2000);
-                console.log(values.bang);
-                console.log(values.cot);
-                console.log(values.dieukien);
-                console.log(values.bangvitu1);
-                console.log(values.cotvitu1);
-                console.log(values.dieukienvitu1);
-                const body = {
-                    "bang": values.bang,
-                    "cot": values.cot,
-                    "dieukien": values.dieukien,
-                    "bangvitu1": "nhanvien",
-                    "cotvitu1": "MaCV",
-                    "dieukienvitu1": values.dieukienvitu1
+                if (isLogin === false) {
+                    let columnsTemps = []
+
+                    const res = await ServiceDistributed.ShowTable(database)
+                    if (res.table) {
+                        res?.table?.map((item, index) => {
+                            columnsTemps.push({
+                                value: item,
+                                label: item,
+                            })
+                        })
+                        setColumnOptionTable(columnsTemps)
+                        message.success("Truy cập thành công")
+                        setIsLogin(true)
+                    } else {
+                        message.warning("Database này không được hỗ trợ phân tán!")
+                    }
+
+
+                } else {
+                    const body = {
+                        "bang": values.bang,
+                        "cot": values.cot,
+                        "dieukien": values.dieukien,
+                        "bangvitu1": "nhanvien",
+                        "cotvitu1": "MaCV",
+                        "dieukienvitu1": values.dieukienvitu1
+                    }
+                    const bodyNull = {
+                        "bang": values.bang,
+                        "cot": values.cot,
+                        "dieukien": values.dieukien,
+                    }
+                    const res = await ServiceDistributed.PhanTanNgang(values.dieukienvitu1 ? body : bodyNull)
+                    message.success('Phân tán dữ liệu thành công');
+                    setColumnOption([])
+                    return true;
                 }
-                const bodyNull = {
-                    "bang": values.bang,
-                    "cot": values.cot,
-                    "dieukien": values.dieukien,
-                }
-                const res = ServiceDistributed.PhanTanNgang(values.dieukienvitu1 ? body : bodyNull)
-                message.success('Phân tán dữ liệu thành công');
-                setColumnOption([])
-                return true;
+
 
             }}
             submitter={{
                 searchConfig: {
-                    submitText: 'Xác nhận',
-                    resetText: 'Hủy bỏ',
+                    submitText: isLogin === false ? "Kết nối" : 'Xác nhận',
+                    resetText: isLogin === false ? "Hủy" : 'Đóng kết nối',
                 },
             }}
         >
-            <ProForm.Group>
+            {isLogin === false && (
+                <ProForm.Group>
+                    <ProFormText
+                        width="lg"
+                        name="host"
+                        onChange={(e) => setHost(e.target.value)}
+                        label="Host"
+                        placeholder="Nhập host"
+                    />
+                    <ProFormText
+                        width="lg"
+                        name="username"
+                        onChange={(e) => setUserName(e.target.value)}
+                        label="User name"
+                        placeholder="Nhập User name"
+                    />
+                    <ProFormText.Password
+                        label="Password"
+                        width="lg"
+                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Nhập Password"
+                        fieldProps={{
+                            size: 'large',
+                            prefix: <AiFillLock className={'prefixIcon'} />,
+                        }}
+                    />
 
-                <ProFormSelect
-                    options={tableOption}
-                    onChange={(value) => setBang(value)}
-                    width="sm"
-                    name="bang"
-                    label="Tên bảng"
-                    placeholder="Chọn bảng"
-                />
-                <ProFormSelect
-                    value={cot}
-                    options={columnOption}
-                    onChange={(value) => setCot(value)}
-                    width="sm"
-                    name="cot"
-                    label="Cột"
-                    placeholder="Chọn cột"
-                />
-                <ProFormText
-                    width="sm"
-                    onChange={(e) => setDieukien(e.target.value)}
-                    name="dieukien"
-                    label="Điều kiện"
-                    placeholder="Nhập điều kiện"
-                />
-            </ProForm.Group>
+                    <Button type='primary' onClick={loginMysql}>Đăng nhập</Button>
 
-            <ProForm.Group>
-                <ProFormText
-                    width="sm"
-                    value="NhanVien"
-                    name="bangvitu1"
-                    label="Bảng vị từ"
-                    disabled
-                />
-                <ProFormText
-                    width="sm"
-                    value="MaCV"
-                    name="cotvitu1"
-                    label="Cột vị từ"
-                    disabled
-                />
-                <ProFormText
-                    width="sm"
-                    onChange={(value) => setDieukienvitu1(value)}
-                    name="dieukienvitu1"
-                    label="Điều kiện vị từ"
-                    placeholder="Nhập điều kiện vị từ"
-                />
-            </ProForm.Group>
-            <Card title="Truy vấn" bordered={false} style={{ width: "100%" }}>
-                Select  <strong>{`${bang ? bang : ""}`}</strong> from <strong>{`${bang ? bang : ""}`}</strong> {" where "}
-                <strong>{`${cot ? cot : ""}`}</strong> = <strong>{`'${dieukien ? dieukien : ""}'`}</strong>
-            </Card>
+
+                    <ProFormSelect
+                        width="lg"
+                        onChange={(value) => setDataBase(value)}
+                        name="database"
+                        label="Database"
+                        placeholder="Chọn database"
+                        options={columnOptionDatabase}
+                    />
+                </ProForm.Group>
+            )}
+
+
+            {
+                isLogin === true && (
+                    <>
+                        <ProForm.Group>
+
+                            <ProFormSelect
+                                options={columnOptionTable}
+                                onChange={(value) => setBang(value)}
+                                width="sm"
+                                name="bang"
+                                label="Tên bảng"
+                                placeholder="Chọn bảng"
+                            />
+                            <ProFormSelect
+                                value={cot}
+                                options={columnOption}
+                                onChange={(value) => setCot(value)}
+                                width="sm"
+                                name="cot"
+                                label="Cột"
+                                placeholder="Chọn cột"
+
+                                locale={{ emptyText: <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="Danh sách rỗng" /> }}
+                            />
+                            <ProFormText
+                                width="sm"
+                                onChange={(e) => setDieukien(e.target.value)}
+                                name="dieukien"
+                                label="Điều kiện"
+                                placeholder="Nhập điều kiện"
+                            />
+                        </ProForm.Group>
+                        <ProForm.Group>
+                            <ProFormText
+                                width="sm"
+                                value="NhanVien"
+                                name="bangvitu1"
+                                label="Bảng vị từ"
+                                disabled
+                            />
+                            <ProFormText
+                                width="sm"
+                                value="MaCV"
+                                name="cotvitu1"
+                                label="Cột vị từ"
+                                disabled
+                            />
+                            <ProFormText
+                                width="sm"
+                                name="dieukienvitu1"
+                                label="Điều kiện vị từ"
+                                placeholder="Nhập điều kiện vị từ"
+                            />
+                        </ProForm.Group>
+
+                        <Card title="Truy vấn" bordered={false} style={{ width: "100%" }}>
+                            Select  <strong>{`${bang ? bang : ""}`}</strong> from <strong>{`${bang ? bang : ""}`}</strong> {" where "}
+                            <strong>{`${cot ? cot : ""}`}</strong> = <strong>{`'${dieukien ? dieukien : ""}'`}</strong>
+                        </Card>
+                    </>
+                )
+            }
+
 
         </ModalForm >
     );
--- src/service/ServiceDistributed.js
+++ src/service/ServiceDistributed.js
@@ -4,11 +4,14 @@ const ServiceDistributed = {
     PhanTanNgang: async (body) => {
         return requests.post(`/phantan`, body);
     },
-    ShowTable: async () => {
-        return requests.get(`/mysql/showtable`);
+    ShowTable: async (database) => {
+        return requests.get(`/mysql/showtable/${database}`);
     },
     ShowColumn: async (table) => {
         return requests.get(`/mysql/showcolumn/${table}`);
+    },
+    LoginMysql: async (body) => {
+        return requests.post(`/mysql/login`, body);
     },
 }
 
